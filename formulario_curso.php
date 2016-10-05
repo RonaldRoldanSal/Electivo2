6932a063b07bd13006539a69d6657997aa27a6c8
LabCom <LabCom@DESKTOP-R4U7GJA>
2016-10-05 15:19:28 -0500

    Enviado

--- formulario_curso.php
+++ formulario_curso.php
@@ -1,8 +1,53 @@
-<?php
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Practica 07</title>
+    </head>
+    <body>
+        <form method="POST">
+            <fieldset>
+                <legend>:: Registro de Curso</legend>
+                <table>
+                    <tr>
+                        <td>Código: </td>
+                        <td>
+                            <input type="text" size="10" name="txtCodigo" size="25"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Nombre: </td>
+                        <td>
+                            <input type="text" size="10" name="txtNombre" size="25"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Horas: </td>
+                        <td>
+                            <input type="text" size="10" name="txtHoras" size="25"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Credito: </td>
+                        <td>
+                            <input type="text" size="10" name="txtCredito" size="25"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" name="btnEnviar" value="Enviar"/>
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>
+        </form>
+    </body>
+</html>
+        <?php
+       
+        
+        ?>
+    </body>
+</html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
